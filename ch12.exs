85ffebab62aff87a9d87c1238d5e75184185a157
Jonathan Stoker <121897095+jonathan-stoker@users.noreply.github.com>
2024-07-05 11:20:01 -0700

    Progress chapter 12 ex 1

--- ch12.exs
+++ ch12.exs
@@ -2,5 +2,18 @@ defmodule ControlFlowChapter do
 
 # Rewrite the FizzBuzz example using case.
 
+def case_test(number) do
+  case number do
+    check_fizzbuzz = number when rem(number, 3) == 0 and rem(number, 5) == 0 ->
+      "FizzBuzz"
+    check_fizz = number when rem(number, 5) == 0 ->
+      "Fizz"
+    check_buzz = number when rem(number, 3) == 0 ->
+      "Buzz"
+    _ ->
+      number
+    end
+end
+
 
 end
