8c508c478bb028f171c788811428c940d1d2a038
Jonathan Stoker <121897095+jonathan-stoker@users.noreply.github.com>
2024-07-06 18:37:35 -0700

    Solution Chapter 12 ex 1

--- ch12.exs
+++ ch12.exs
@@ -1,14 +1,18 @@
 defmodule ControlFlowChapter do
+# Chapter 12 (Programming Elixir)
+# Exercise 1: Rewrite the FizzBuzz example using case.
 
-# Rewrite the FizzBuzz example using case.
+def fizzbuzz(number) when number > 0 do
+  1..number |> Enum.map(&fb_case/1)
+end
 
-def case_test(number) do
+defp fb_case(number) do
   case number do
-    check_fizzbuzz = number when rem(number, 3) == 0 and rem(number, 5) == 0 ->
+    number when rem(number, 3) == 0 and rem(number, 5) == 0 ->
       "FizzBuzz"
-    check_fizz = number when rem(number, 5) == 0 ->
+    number when rem(number, 5) == 0 ->
       "Fizz"
-    check_buzz = number when rem(number, 3) == 0 ->
+    number when rem(number, 3) == 0 ->
       "Buzz"
     _ ->
       number
